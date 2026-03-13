00c1ecb1e63a8023ac46b6ae56f9d378935afd04
ihundal8478 <ihundal8478@conestogac.on.ca>
2026-03-13 13:35:31 -0400

    functions added to pass the tests

--- Game/game.c
+++ Game/game.c
@@ -1,15 +1,40 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+#include<string.h>
 #include "game.h"
 
 const char* playGame(const char* player1Shape, const char* player2Shape)
 {
-	//#write functional code here
-	//return STR_INVALID;
+
+	if (strcmp(player1Shape, STR_ROCK) == 0 && strcmp(player2Shape, STR_ROCK) == 0)
+		return STR_DRAW;
+
+	if (strcmp(player1Shape, STR_ROCK) == 0 && strcmp(player2Shape, STR_SISSOR) == 0)
+		return STR_PLAYER1;
+
+	if (strcmp(player1Shape, STR_ROCK) == 0 && strcmp(player2Shape, STR_PAPER) == 0)
+		return STR_PLAYER2;
+
+	if (strcmp(player1Shape, STR_SISSOR) == 0 && strcmp(player2Shape, STR_PAPER) == 0)
+		return STR_PLAYER1;
+
+	if (strcmp(player1Shape, STR_SISSOR) == 0 && strcmp(player2Shape, STR_ROCK) == 0)
+		return STR_PLAYER2;
+
+	if (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_ROCK) == 0)
+		return STR_PLAYER1;
+
+	if (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_SISSOR) == 0)
+		return STR_PLAYER2;
+
+	if (strcmp(player1Shape, STR_SISSOR) == 0 && strcmp(player2Shape, STR_SISSOR) == 0)
+		return STR_DRAW;
+
+	if (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_PAPER) == 0)
+		return STR_DRAW;
+
+	return STR_INVALID;
 }
 
 int main()
 {
-    return 1;
+	return 1;
 }
